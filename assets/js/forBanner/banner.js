3d8b84141c1a38c8e2340cc13151c341e2f8b9f2
izvolte <50668641+izvolte@users.noreply.github.com>
2021-06-25 10:45:12 +0300

    add banner and fix product review

---
+++ assets/js/forBanner/banner.js
@@ -0,0 +1,86 @@
+//
+//   Variables
+//
+//////////////////////////////////////////////////////////////////////
+let tickerSpeed = 0
+
+// Play with this value to change the speed
+if (document.documentElement.clientWidth > 700) {
+    tickerSpeed = -1.5;
+} else {
+    tickerSpeed = -0.5;
+}
+
+let flickity = null;
+let isPaused = false;
+const slideshowEl = document.querySelector('.top-banner__ticker');
+
+
+//
+//   Functions
+//
+//////////////////////////////////////////////////////////////////////
+
+const update = () => {
+    if (isPaused) return;
+    if (flickity.slides) {
+        flickity.x = (flickity.x - tickerSpeed) % flickity.slideableWidth;
+        flickity.selectedIndex = flickity.dragEndRestingSelect();
+        flickity.updateSelectedSlide();
+        flickity.settle(flickity.x);
+    }
+    window.requestAnimationFrame(update);
+};
+
+const pause = () => {
+    isPaused = true;
+};
+
+const play = () => {
+    if (isPaused) {
+        isPaused = false;
+        window.requestAnimationFrame(update);
+    }
+};
+
+
+//
+//   Create Flickity
+//
+//////////////////////////////////////////////////////////////////////
+
+flickity = new Flickity(slideshowEl, {
+    autoPlay: false,
+    pageDots: false,
+    draggable: false,
+    wrapAround: true,
+    selectedAttraction: 0.015,
+    friction: 0.25,
+    prevNextButtons: false
+});
+flickity.x = 0;
+
+
+//
+//   Add Event Listeners
+//
+//////////////////////////////////////////////////////////////////////
+
+flickity.on('dragStart', () => {
+    isPaused = true;
+});
+
+
+//
+//   Start Ticker
+//
+//////////////////////////////////////////////////////////////////////
+
+update();
+
+
+document.addEventListener('click', ({target}) => {
+    if (target.closest('.top-banner__close')) {
+        target.closest('.top-banner').style.height = '0'
+    }
+})
